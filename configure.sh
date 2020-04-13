f39c1ee2258972f5b08842b7e85816e5e0298d49
chouzee <anton.ryabchenko.92@gmail.com>
2020-04-13 18:19:46 +0300

    add i3

--- configure.sh
+++ configure.sh
@@ -1,26 +1,39 @@
-#!/bin/bash
 
-function system_update() {
-    sudo apt update && sudo apt upgrade -y
+n update() {
+  sudo apt update && \
+  sudo apt upgrade -y
 }
-system_update
+update
 
-echo "Installing i3...."
-read -p "Would you like to use i3 along with your DE?: " $answer
-if [[ $answer = 'yes' ]]; 
-then
-   i3_de()
-else
-    i3_server()
-
-# i3 installing
-function i3_server() {
-    sudo apt install i3 ubuntu-drivers-common mesa-utils \
-    mesa-utils-extra gnupg numlockx xautolock scrot xorg \
-    xserver-xorg wget unzip wpasupplicant
-}
-
-function i3_de() {
+function i3install() {
+  read -p "Would you like to install i3 along with DE? " answer
+  if [[ $answer == 'yes' ]]
+  then
     sudo apt install i3
+  elif [[ $answer == 'no' ]]
+  then
+    sudo apt install i3 -y && \
+    sudo apt install xinit -y
+  else
+    echo "You should choose yes or no"
+  fi
 }
+i3install
+
+
+function i3autostart() {
+  read -p "Would you like to start i3 automatically? " answer
+  if [[ $answer == 'yes' ]]
+  then
+    echo "
+    # autostart i3
+    if [[ "$(tty)" == '/dev/tty1' ]]; then
+      exec startx
+    fi" | sudo tee -a /etc/profile
+    elif [[ $answer == 'no' ]]
+    then
+      echo "Don't forget to run i3"
+   fi
+}
+i3autostart
 
